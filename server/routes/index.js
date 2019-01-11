d1db789563b54d78870da1b1edeae8d071f475bf
Fred Mottey <FMottey@rbaconsulting.com>
2019-01-11 16:53:44 -0500

    expressjs

---
+++ server/routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const router = express.Router();
+
+const speakersRoute = require('./speakers');
+const feedbacksRoute = require('./feedback');
+
+module.exports = () => {
+    router.get('/', (req, res, next)=>{
+        return res.render('index');
+
+    })
+
+    router.use('/speakers', speakersRoute());
+    router.use('/feedback', feedbacksRoute());
+
+    return router;
+};
